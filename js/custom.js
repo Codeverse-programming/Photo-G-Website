71fe74947b5b0f7e0f4e63d09af00d90e87e9471
programmingwithVee <adannabassey0@gmail.com>
2023-03-09 15:35:21 +0100

    Photo-G Website

--- js/custom.js
+++ js/custom.js
@@ -30,14 +30,11 @@ document.addEventListener("DOMContentLoaded", () => {
         sarahSection.classList.add("form--hidden");
     });
 
-});
-
-document.addEventListener("DOMContentLoaded", () => {
     const shippingDetails = document.querySelector("#shipping-section")
     const orderSummary = document.querySelector("#order-summary-section")
     const confirmDetails = document.querySelector("#confirm-details")
     const paymentDetails = document.querySelector("#payment-details-section")
-
+    
     document.querySelector("#linkShipping").addEventListener("click", e => {
         e.preventDefault();
         shippingDetails.classList.remove("form--hidden")
@@ -45,14 +42,14 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmDetails.classList.add("form--hidden")
         paymentDetails.classList.add("form--hidden")
     });
-
+    
     document.querySelector("#linkSummary").addEventListener("click", e => {
         e.preventDefault();
         shippingDetails.classList.add("form--hidden")
         orderSummary.classList.remove("form--hidden")
         paymentDetails.classList.add("form--hidden")
     });
-
+    
     document.querySelector("#linkConfirm").addEventListener("click", e => {
         e.preventDefault();
         shippingDetails.classList.add("form--hidden")
@@ -60,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmDetails.classList.remove("form--hidden")
         paymentDetails.classList.add("form--hidden")
     });
-
+    
     document.querySelector("#linkPayment").addEventListener("click", e => {
         e.preventDefault();
         shippingDetails.classList.add("form--hidden")
@@ -68,38 +65,60 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmDetails.classList.add("form--hidden")
         paymentDetails.classList.remove("form--hidden")
     });
+
+    document.querySelector("#btn").addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector("#cardImage").style.border = "10px solid brown"
+        document.querySelector("#cardImage").style.borderRadius = "25px"
+        // document.querySelector(".text").textContent = "Unselect Photo"
+    });
+    
+    document.querySelector(".text").addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector("#cardImage").style.border = "none"
+        document.querySelector("#cardImage").style.borderRadius = "0"
+    });
+    
+    document.querySelector("#button").addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector("#cardImages").style.border = "10px solid brown"
+        document.querySelector("#cardImages").style.borderRadius = "25px"
+        // document.querySelector(".text").textContent = "Unselect Photo"
+    });
+    
+    document.querySelector("#button1").addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector("#cardImage1").style.border = "10px solid brown"
+        document.querySelector("#cardImage1").style.borderRadius = "25px"
+        // document.querySelector(".texted").textContent = "Unselect Photo"
+    });
+    
+    document.querySelector("#button2").addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector("#cardImage2").style.border = "10px solid brown"
+        document.querySelector("#cardImage2").style.borderRadius = "25px"
+        // document.querySelector(".text2").textContent = "Unselect Photo"
+    });
 });
 
-document.querySelector("#btn").addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector("#cardImage").style.border = "10px solid brown"
-    document.querySelector("#cardImage").style.borderRadius = "25px"
-    // document.querySelector(".text").textContent = "Unselect Photo"
-});
+// document.addEventListener("DOMContentLoaded", () => {
+// });
 
-document.querySelector(".text").addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector("#cardImage").style.border = "none"
-    document.querySelector("#cardImage").style.borderRadius = "0"
-});
 
-document.querySelector("#button").addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector("#cardImages").style.border = "10px solid brown"
-    document.querySelector("#cardImages").style.borderRadius = "25px"
-    // document.querySelector(".text").textContent = "Unselect Photo"
-});
 
-document.querySelector("#button1").addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector("#cardImage1").style.border = "10px solid brown"
-    document.querySelector("#cardImage1").style.borderRadius = "25px"
-    // document.querySelector(".texted").textContent = "Unselect Photo"
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const package1 = document.querySelector("#package1")
+    const package2 = document.querySelector("#package2")
 
-document.querySelector("#button2").addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector("#cardImage2").style.border = "10px solid brown"
-    document.querySelector("#cardImage2").style.borderRadius = "25px"
-    // document.querySelector(".text2").textContent = "Unselect Photo"
+    document.querySelector("#linkPackage2").addEventListener("click", e => {
+        e.preventDefault();
+        package1.classList.add("form--hidden");
+        package2.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkPackage1").addEventListener("click", e => {
+        e.preventDefault();
+        package1.classList.remove("form--hidden");
+        package2.classList.add("form--hidden");
+    });
 });
---
+++ js/selectPhotos.js
@@ -0,0 +1,120 @@
+count = 0;
+
+function counterFunc() {
+    count++;
+    document.getElementById("count").innerHTML = count;
+    document.getElementById("click-button").style.display = "none";
+
+    // if (typeof (Storage) !== "undefined") {
+    //     if (localStorage.count) {
+    //         localStorage.count = Number(localStorage.count) + 1;
+    //     } else {
+    //         localStorage.count = 1;
+    //     }
+    //     document.getElementById("count").innerHTML = localStorage.count;
+    //     document.getElementById("click-button").style.display ="none"
+    //     // document.getElementById("select-button").style.display ="block"
+    // } else {
+    //     document.getElementById("count").innerHTML = "Sorry, the browser you used does not support web storage.";
+    // }
+}
+
+function hideFunc() {
+    count--;
+    document.getElementById("count").innerHTML = count;
+
+    // if (typeof (Storage) !== "undefined") {
+    //     if (localStorage.count) {
+    //         localStorage.count = Number(localStorage.count) - 1;
+    //     } else {
+    //         localStorage.count = 1;
+    //     }
+    //     document.getElementById("count").innerHTML = localStorage.count;
+    //     // document.getElementById("select-button").style.display = "block"
+    // } else {
+    //     document.getElementById("count").innerHTML = "Sorry, the browser you used does not support web storage.";
+    // }
+}
+
+// document.querySelector(".clickedButton").addEventListener("click", e => {
+//     e.preventDefault();
+//     document.querySelector(".showButton").style.backgroundColor = "red"
+// });
+// document.querySelector(".clickedBtn").addEventListener("click", e => {
+//     e.preventDefault();
+//     document.querySelector(".showButton").style.background = "green"
+// });
+
+function changeColor(color) {
+    document.querySelector(".showM").style.background = color;
+}
+function moveColor(colored) {
+    document.querySelector(".showS").style.background = colored;
+}
+function freeColor(color1) {
+    document.querySelector(".showL").style.background = color1;
+}
+function sizeColor(color2) {
+    document.querySelector(".sizeS").style.border = color2;
+}
+function size1Color(color3) {
+    document.querySelector(".sizeL").style.border = color3;
+}
+function size2Color(color4) {
+    document.querySelector(".sizeM").style.border = color4;
+}
+function size3Color(color5) {
+    document.querySelector(".sizeA").style.border = color5;
+}
+function size4Color(color6) {
+    document.querySelector(".sizeB").style.border = color6;
+}
+function size5Color(color7) {
+    document.querySelector(".sizeC").style.border = color7;
+}
+function gfg_Run() {
+    changeColor('red');
+    moveColor('none');
+    freeColor('none');
+    // el_down.innerHTML = "Background Color changed";
+}
+function mfg_Run() {
+    moveColor('red');
+    changeColor('none');
+    freeColor('none');
+}
+function ffg_Run() {
+    moveColor('none');
+    changeColor('none');
+    freeColor('red');
+}
+function size3_Run() {
+    size3Color('none');
+    size4Color('none');
+    size5Color('none');
+}
+function size4_Run() {
+    size3Color("red");
+    size4Color("none");
+    size5Color("none");
+}
+function size5_Run() {
+    size3Color("none");
+    size4Color("red");
+    size5Color("none");
+}
+function size_Run() {
+    sizeColor("5px solid red");
+    size1Color("1px solid black");
+    size2Color("1px solid black");
+}
+function size1_Run() {
+    sizeColor("1px solid black");
+    size1Color("5px solid red");
+    size2Color("1px solid black");
+}
+function size2_Run() {
+    sizeColor("1px solid black");
+    size1Color("1px solid black");
+    size2Color("5px solid red");
+}
